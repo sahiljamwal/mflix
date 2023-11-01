8b2d12f4ea62b7a0c948bab047cae794a5c0747f
sahiljamwal <sahil.jamwal78625@gmail.com>
2023-11-01 15:47:49 +0530

    update : master | init commit

---
+++ src/express.ts
@@ -0,0 +1,38 @@
+import express, { json, Request, Response, urlencoded } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import compression from "compression";
+import apiRouter from "./routes";
+
+const app = express();
+
+app.use(helmet());
+app.use(
+  compression({
+    filter: (req: Request, res: Response) =>
+      req.headers["x-no-compression"] ? false : compression.filter(req, res),
+  })
+);
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: false }));
+
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+    ].join(" ");
+  })
+);
+
+// API entrypoint
+app.use("/api", apiRouter);
+
+export default app;
---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import express, { Request, Response } from "express";
+
+const apiRouter = express.Router();
+
+apiRouter.get("/health", (_req: Request, res: Response) => res.sendStatus(200));
+
+export default apiRouter;
